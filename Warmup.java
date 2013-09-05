0142befa282dff6c69500ecdfba2929b586b57a8
apcs <apcs@mvhs-u528-2.dcsdk12.org>
2013-09-05 09:16:35 -0600

    Added Factorial

--- Warmup.java
+++ Warmup.java
@@ -7,7 +7,8 @@ public class Warmup {
 	  // 	System.out.println("I got it right!");
 	  // }
       
-      exponent(2,3);
+      // exponent(2,3);
+		factorial (4);
 
 	}
 
@@ -29,7 +30,12 @@ public class Warmup {
 	}
 
 	public static int factorial(int n) {
-		int factorial=0;
+		int factorial=1;
+		for (int i =1; i <=n; i++) {
+			factorial = factorial *i;
+		}
+		System.out.println(factorial);
+		return factorial;
 	}
 	
 }
